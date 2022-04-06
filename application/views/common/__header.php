bc9a9acaa4e9974eb2a0baa96bf8d7ff1b7508a4
ahbabrasul <ahbabrasul@icloud.com>
2022-04-06 09:23:15 +0300

    auth - stage 1

--- application/views/common/__header.php
+++ application/views/common/__header.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-    <title>Quick Loans</title>
+    <title>IGMS</title>
 
     <meta name="description" content="Quick Loans">
     <meta name="author" content="ahbabrasul">
